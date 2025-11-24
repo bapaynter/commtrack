7be6cc8222793cfa6327fb3d310b71cd06b9515c
Bryce <bpaynter@pharmetika.com>
2025-11-24 14:24:38 -0600

    update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: "commtrack",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
-      instances: 4,
+      script: "npm",
+      args: "run start -- -p 3130",
+      instances: 1,
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
